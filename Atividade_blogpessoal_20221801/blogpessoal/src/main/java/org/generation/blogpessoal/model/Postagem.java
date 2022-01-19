b8172f5aa00ce3d5dde4ba92c1ac63bfdd3451c3
Gabriel Luis Nolasco <gl450681@gmail.com>
2022-01-19 12:56:15 -0300

    hotfix 20221901
    hotfix post name

--- Atividade_blogpessoal_20221801/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ Atividade_blogpessoal_20221801/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -13,48 +13,55 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "tb_postagens")
+@Table(name = "tb_posts")
 public class Postagem {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id_postagem;
+	private long idPost;
 	
 	@NotNull
 	@Size(min = 8, max = 128)
-	private String titulo_postagem;
+	private String titlePost;
 	
 	@NotNull
 	@Size(min = 32, max = 512)
-	private String texto_postagem;
+	private String textPost;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date date_postagem = new java.sql.Date(System.currentTimeMillis());
-	
-	public long getId_postagem() {
-		return id_postagem;
+	private Date datePost = new java.sql.Date(System.currentTimeMillis());
+
+	public long getIdPost() {
+		return idPost;
 	}
-	public void setId_postagem(long id_postagem) {
-		this.id_postagem = id_postagem;
+
+	public void setIdPost(long idPost) {
+		this.idPost = idPost;
 	}
-	public String getTitulo_postagem() {
-		return titulo_postagem;
+
+	public String getTitlePost() {
+		return titlePost;
 	}
-	public void setTitulo_postagem(String titulo_postagem) {
-		this.titulo_postagem = titulo_postagem;
+
+	public void setTitlePost(String titlePost) {
+		this.titlePost = titlePost;
 	}
-	public String getTexto_postagem() {
-		return texto_postagem;
+
+	public String getTextPost() {
+		return textPost;
 	}
-	public void setTexto_postagem(String texto_postagem) {
-		this.texto_postagem = texto_postagem;
+
+	public void setTextPost(String textPost) {
+		this.textPost = textPost;
 	}
-	public Date getDate_postagem() {
-		return date_postagem;
+
+	public Date getDatePost() {
+		return datePost;
 	}
-	public void setDate_postagem(Date date_postagem) {
-		this.date_postagem = date_postagem;
+
+	public void setDatePost(Date datePost) {
+		this.datePost = datePost;
 	}
-	
+
 	
 }
